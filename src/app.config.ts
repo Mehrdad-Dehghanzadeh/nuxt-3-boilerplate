04523d482a33b5baa1318ffe9c97f720df784197
Mehrdad Dehghanzadeh <77623680+Mehrdad-Dehghanzadeh@users.noreply.github.com>
2023-02-19 10:38:46 +0330

    Dev (#5)
    * chore: update nuxt
    
    * chore: update nuxt
    
    * feat: add app.config file

---
+++ src/app.config.ts
@@ -0,0 +1,7 @@
+//Do not put any secret values inside app.config file. It is exposed to the user client bundle
+
+export default defineAppConfig({
+  publicUrl: '',
+  appVersion: '0.0.0',
+  apiBaseUrl: ''
+})
