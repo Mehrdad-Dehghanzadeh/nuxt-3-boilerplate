b6053bf9909717b78ebf46b48dd2d3e484be7fc0
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2023-06-22 14:44:50 +0430

    feat: add global import apis folders

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,14 +2,18 @@
   <article></article>
 </template>
 
-<script>
+<script lang="ts">
+import LoginDto from '@dtos/LoginDto'
+
 export default {
   name: 'HomePage',
 
   mounted() {
+    const payload: LoginDto = { username: 'a@a.com', password: '123456' }
+
     setTimeout(() => {
       this.$api.auth
-        .login({ username: 'a@a.com', password: '123456' })
+        .login(payload)
         .then((res) => {
           console.log(res)
         })
