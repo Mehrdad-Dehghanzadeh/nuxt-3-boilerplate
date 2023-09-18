dd99da93402f00bb8119c92c59e62a7ad2d809c7
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2023-09-18 16:33:17 +0330

    feat: add nuxt loading

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,17 @@
 <template>
   <v-app>
-    <nuxt-layout />
+    <nuxt-layout>
+      <nuxt-loading-indicator />
+
+      <nuxt-page />
+    </nuxt-layout>
   </v-app>
 </template>
 
 <script lang="ts" setup>
 useHead({
   titleTemplate: (titleChunk) => {
-    return titleChunk ? `${titleChunk} | Nuxt 3 Boilerplate` : 'Nuxt 3 Boilerplate';
+    return titleChunk ? `${titleChunk} | Nuxt 3 Boilerplate` : 'Nuxt 3 Boilerplate'
   }
 })
 </script>
