a129f20fd632c8c22147b5b740cd2879bf5d2e09
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2023-09-19 17:11:15 +0330

    chore: initial for colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,5 +1,17 @@
-import { createVuetify } from 'vuetify'
+import { createVuetify, ThemeDefinition } from 'vuetify'
 import { fa } from 'vuetify/locale'
+import { BaseColors } from '@types/Colors'
+
+const lightColors: Readonly<BaseColors> = {
+  background: '#fff',
+  surface: '#fff',
+  primary: '#fff',
+  secondary: '#fff',
+  success: '#fff',
+  warning: '#fff',
+  error: '#fff',
+  info: '#fff'
+}
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
@@ -8,6 +20,14 @@ export default defineNuxtPlugin((nuxtApp) => {
       locale: 'fa',
       messages: { fa },
       rtl: { fa: true }
+    },
+
+    theme: {
+      themes: {
+        light: {
+          colors: lightColors
+        }
+      }
     }
   })
 
