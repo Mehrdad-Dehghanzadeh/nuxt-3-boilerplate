251c1f1fdfa28682b889b6dabc2565b3658212c1
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2023-10-08 11:17:49 +0330

    feat: KTextCopy component

---
+++ src/components/kits/KCopyText/KCopyText.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="k-copy-text" @click="copy(text)">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KCopyText',
+
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+
+    message: {
+      type: String,
+      default: 'متن کپی شد'
+    }
+  },
+
+  methods: {
+    copy() {
+      copyText(this.text)
+      useSnackbar('success', this.message)
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./KCopyText.scss" />
