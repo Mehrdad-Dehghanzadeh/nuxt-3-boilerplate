da3b155ce8cefb0e3dbdedf3669379e8a2069e5d
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2023-10-11 15:42:44 +0330

    chore: initial page error

---
+++ src/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-theme-provider>
+    <v-app>
+      <v-layout class="error-page flex-column" full-height>
+        <div class="text-center">
+          <h2 class="mt-10">{{ error?.statusCode }}</h2>
+          <h2 class="mt-4">{{ error }}</h2>
+        </div>
+      </v-layout>
+    </v-app>
+  </v-theme-provider>
+</template>
+
+<script lang="ts" setup>
+const error = useError()
+
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} | Nuxt 3 Boilerplate` : 'Nuxt 3 Boilerplate'
+  },
+
+  title: 'Error'
+})
+</script>
