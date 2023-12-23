102e9157817b20195ab50c7db26fd78be37957ba
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2023-12-23 15:12:22 +0330

    feat: add enums functions

--- src/utils/enumsProvider.ts
+++ src/utils/enumsProvider.ts
@@ -27,3 +27,7 @@ export default function enumsProvider(
 
   return typeof item !== 'undefined' ? item : {}
 }
+
+export function enums(type: keyof typeof enumsList): EnumType[] {
+  return enumsList[type]
+}
