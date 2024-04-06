e3f6adf6c418ed7976fde11f949762129b940f14
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2024-04-07 00:27:02 +0330

    chore: reslove confilctd

--- src/assets/api-plugin/autoImportApis.ts
+++ src/assets/api-plugin/autoImportApis.ts
@@ -3,7 +3,9 @@ import { type AxiosInstance } from 'axios'
 export default function (api: any, $axios: AxiosInstance) {
   const apisModules: any = import.meta.glob(`../../apis/*.(ts|js)`, { eager: true })
 
-  for (const apiFile in apisModules) {const fileName: string = apiFile.replace('../../apis', '')
+  for (const apiFile in apisModules) {
+    const fileName: string = apiFile
+      .replace('../../apis', '')
       .replaceAll('/', '')
       .replace('.js', '')
       .replace('.ts', '')
