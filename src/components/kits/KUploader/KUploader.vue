47023bcbb08af3da34cea4faacd59cf093fd0c2c
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2024-04-09 11:49:45 +0330

    feat: add uploader

--- src/components/kits/KUploader/KUploader.vue
+++ src/components/kits/KUploader/KUploader.vue
@@ -1,27 +1,25 @@
 <template>
   <div :class="['k-uploader', className]">
     <v-input ref="vNode" :model-value="modelValue" :rules="props.rules" v-bind="attrs$">
-      <template #default="{}">
-        <div v-if="hasFile && vNode?.isValid" class="k-uploader-has-file"></div>
+      <div v-if="hasFile && vNode?.isValid" class="k-uploader-has-file"></div>
 
-        <div v-else :class="['k-uploader__wrapper', {}]" @click="open">
-          <v-icon class="k-uploader__icon" color="primary" size="64">
-            mdi-cloud-upload
-          </v-icon>
-          <small class="k-uploader__placeholder">{{ safePlaceholder }}</small>
-        </div>
+      <div v-else :class="['k-uploader__wrapper', {}]" @click="open">
+        <v-icon class="k-uploader__icon" color="primary" size="64">
+          mdi-cloud-upload
+        </v-icon>
+        <small class="k-uploader__placeholder">{{ safePlaceholder }}</small>
+      </div>
 
-        <client-only>
-          <input
-            class="k-uploader__input"
-            :name="safeName"
-            :id="safeId"
-            type="file"
-            ref="input"
-            @change="change"
-          />
-        </client-only>
-      </template>
+      <client-only>
+        <input
+          class="k-uploader__input"
+          :name="safeName"
+          :id="safeId"
+          type="file"
+          ref="input"
+          @change="change"
+        />
+      </client-only>
     </v-input>
   </div>
 </template>
@@ -42,6 +40,7 @@ const { safeId, safeName, safePlaceholder } = useControl()
 const input = ref<HTMLInputElement>()
 const hasFile = ref<boolean>()
 let modelValue = ref<any>(null)
+const model = defineModel<any>()
 const vNode = ref()
 
 function open() {
@@ -53,17 +52,36 @@ function updateValue() {
   hasFile.value = !!input.value?.files?.[0]
 }
 
-function readFile() {
-  toBase64(input.value?.files?.[0]).then((data) => {
-    console.log(data)
-  })
+function isValid() {
+  return vNode.value
+    ?.validate()
+    .then((res: string[]) =>
+      Array.isArray(res) && !res?.length
+        ? Promise.resolve()
+        : Promise.reject(new Error('is not Valid'))
+    )
+}
+
+async function saveBase64() {
+  try {
+    await isValid()
+    await toBase64(input.value?.files?.[0]).then((data) => {
+      model.value = data
+      return data
+    })
+  } catch (error: any) {
+    console.log(error)
+  }
 }
 
 async function change() {
   updateValue()
   await nextTick()
-  readFile()
 }
+
+defineExpose({
+  saveBase64
+})
 </script>
 
 <style src="./KUploader.scss" lang="scss" />
