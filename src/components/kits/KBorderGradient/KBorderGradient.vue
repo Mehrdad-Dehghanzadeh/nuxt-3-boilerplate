248c1b6356dfda6f956efd339c546ad7dbc89ea7
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2025-02-01 22:31:47 +0330

    chore: initial for component

---
+++ src/components/kits/KBorderGradient/KBorderGradient.vue
@@ -0,0 +1,15 @@
+<template>
+  <component class="k-border-gradient" :is="props.tag">
+    <slot name="default" />
+  </component>
+</template>
+
+<script setup lang="ts">
+import type { Props } from './types'
+const props = withDefaults(defineProps<Props>(), {
+  borderSize: '1px',
+  tag: 'div'
+})
+</script>
+
+<style lang="sass" src="./style.scss" />
