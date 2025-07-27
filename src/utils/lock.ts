91ff29603aa639e10fc3b0c93c7f7e299907ff7c
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2025-07-27 14:34:39 +0330

    refactor: call with function

---
+++ src/utils/lock.ts
@@ -0,0 +1,51 @@
+enum LockStatus {
+  Wait = 'wait',
+  Resolve = 'Resolve',
+  Reject = 'Reject',
+  Created = 'Created'
+}
+
+interface ILock {
+  name: string
+  readonly status: LockStatus
+  readonly createdAt: number
+  wait(): Promise<unknown> | undefined
+}
+
+export class Lock implements ILock {
+  name = ''
+  createdAt = 0
+  status = LockStatus.Created
+  #resolve: ((value?: any) => void) | undefined
+  #reject: ((reason?: any) => void) | undefined
+
+  constructor(name: string) {
+    this.name = name
+    this.createdAt = Date.now()
+  }
+
+  wait() {
+    if (this.status != LockStatus.Wait) {
+      this.status = LockStatus.Wait
+
+      return new Promise((res, rej) => {
+        this.#resolve = res
+        this.#reject = rej
+      })
+    }
+  }
+
+  resolve<T = any>(value?: T) {
+    if (this.#resolve) {
+      this.#resolve(value)
+      this.status = LockStatus.Resolve
+    }
+  }
+
+  reject<T = any>(reason?: T) {
+    if (this.#reject) {
+      this.#reject(reason)
+      this.status = LockStatus.Reject
+    }
+  }
+}
